9ecf2e870f5a48f46fafdb17b08c59baf05c1ca7
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-16 16:53:44 -0600

    Add changes to facility class

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,16 +2,36 @@ class Facility
   attr_reader :name, 
               :address, 
               :phone, 
-              :services
+              :services,
+              :registered_vehicles,
+              :collected_fees
 
   def initialize(facility_details)
     @name = facility_details[:name]
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    if @services.include?('Vehicle Registration')
+      if vehicle.electric_vehicle?
+        vehicle.plate_type = :ev
+        @collected_fees += 200
+      elsif vehicle.antique?
+        vehicle.plate_type = :antique
+        @collected_fees += 25
+      else 
+        vehicle.plate_type = :regular
+        @collected_fees +=100
+      end
+      @registered_vehicles << vehicle
+    end
   end
 end
