70e213fd201a14edb2a67706f4b75654c404a37d
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-17 23:50:24 -0600

    Add rest of methods to complete the FacilityFactory class

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -9,10 +9,24 @@ class FacilityFactory
         name: location[:dmv_office],
         address: location[:address_li],
         phone: location[:phone]
-      })
+        })
         #services: location[:services_p].split(",").map(&:strip)})
+      elsif source == "new_york"
+        facility = Facility.new({
+          name: location[:office_name],
+          address: location[:street_address_line_1],
+          phone: location[:public_phone_number],
+        })
+      elsif source == "missouri"
+        facility = Facility.new({
+          name: location[:name],
+          address: location[:address1],
+          phone: location[:phone],
+        })
       end
     end
+
+
 
     
   end
