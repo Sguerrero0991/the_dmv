2484ad9c15ce8671bbd0bf0a57b04d71aa5fd733
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-17 23:10:15 -0600

    add changes to facility_spec

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Facility do
     @registrant_1 = Registrant.new('Bruce', 18, true)
     @registrant_2 = Registrant.new('Penny', 16 )
     @registrant_3 = Registrant.new('Tucker', 15 )
+    @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
   end
 
   describe '#initialize' do
@@ -132,5 +133,27 @@ RSpec.describe Facility do
     @facility_1.renew_drivers_license(@registrant_1)
     expect(@registrant_1.license_data[:renewed]).to eq(true)
   end
+
+  describe '#initialize' do
+    it 'can load colorado dmv office data' do
+    
+      expect(vehicles).to be_an_instance_of(Array)
+
+      expect(vehicles.first).to be_an_instance_of(Vehicle)
+      expect(vehicles.first.vin).to eq("1N4BZ0CP3G")
+      expect(vehicles.first.year).to eq(2016)
+      expect(vehicles.first.make).to eq("NISSAN")
+      expect(vehicles.first.model).to eq("Leaf")
+      expect(vehicles.first.engine).to eq(:ev)
+      
+      expect(vehicles[1]).to be_an_instance_of(Vehicle)
+      expect(vehicles[1].vin).to eq("5YJ3E1EB7K")
+      expect(vehicles[1].year).to eq(2019)
+      expect(vehicles[1].make).to eq("TESLA")
+      expect(vehicles[1].model).to eq("Model 3")
+      expect(vehicles[1].engine).to eq(:ev)
+    end
+
+
 end
 
