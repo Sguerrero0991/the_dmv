ee31228a2a87f12b358e0e7e63b05b77011d5ed0
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-17 23:13:53 -0600

    checked @services instance variable

--- lib/facility.rb
+++ lib/facility.rb
@@ -55,4 +55,16 @@ class Facility
     end
     return registrant.license_data[:renewed] 
   end
+
+  def create_facilities(vehicle_data)
+    vehicle_data.map do |data|
+      vehicle = Vehicle.new({
+        vin: data[:vin_1_10],
+        year: data[:model_year].to_i,
+        make: data[:make], 
+        model:data[:model], 
+        engine: :ev})
+    end
+  end
+
 end
